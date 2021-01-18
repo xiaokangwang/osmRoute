08117174648e91d5b74991c96eae36e36992a59e
Shelikhoo <xiaokangwang@outlook.com>
2021-01-18 23:17:31 +0000

    gRPC interface for map data

--- go.mod
+++ go.mod
@@ -7,7 +7,10 @@ require (
 	github.com/dgraph-io/badger v1.6.2 // indirect
 	github.com/dgraph-io/badger/v2 v2.2007.2
 	github.com/fxamacker/cbor v1.5.1
+	github.com/golang/protobuf v1.4.2
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/paulmach/osm v0.1.1
+	google.golang.org/grpc v1.35.0
+	google.golang.org/protobuf v1.25.0
 	modernc.org/sortutil v1.1.0
 )
---
+++ rpc/grpcs/grpc.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/paulmach/osm"
+	"github.com/xiaokangwang/osmRoute/adm"
+	"github.com/xiaokangwang/osmRoute/mapctx"
+	"github.com/xiaokangwang/osmRoute/rpc"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	mapinde := adm.GetMapFromDir("/run/shm/testdb")
+	mapfile, err := os.Open("/run/shm/ireland.osm.pbf")
+	if err != nil {
+		panic(err)
+	}
+	mapCtx := mapctx.NewMapCtx(*mapinde, mapfile)
+
+	rpc.RegisterRouteServiceServer(grpcServer, &RouteService{mapctx: mapCtx})
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
+
+type RouteService struct {
+	rpc.UnimplementedRouteServiceServer
+	mapctx *mapctx.MapCtx
+}
+
+func (r RouteService) Resolve(ctx context.Context, request *rpc.ObjectResolveRequest) (*rpc.ReturnedObject, error) {
+	obj := r.mapctx.ResolveInfoFromID(request.FeatureID)
+
+	if obj == nil {
+		return &rpc.ReturnedObject{
+			FeatureID:     "",
+			ObjectContent: nil,
+			Found:         false,
+		}, nil
+	}
+	switch (*obj).ObjectID().Type() {
+	case osm.TypeWay:
+		objWay := (*obj).(*osm.Way)
+		by, err := json.Marshal(objWay)
+		if err != nil {
+			panic(err)
+		}
+		println(string(by))
+		return &rpc.ReturnedObject{
+			FeatureID:     request.FeatureID,
+			ObjectContent: by,
+			Found:         true,
+		}, nil
+	case osm.TypeNode:
+		objNode := (*obj).(*osm.Node)
+		by, err := json.Marshal(objNode)
+		if err != nil {
+			panic(err)
+		}
+		println(string(by))
+		return &rpc.ReturnedObject{
+			FeatureID:     request.FeatureID,
+			ObjectContent: by,
+			Found:         true,
+		}, nil
+	case osm.TypeRelation:
+		objRelation := (*obj).(*osm.Relation)
+		by, err := json.Marshal(objRelation)
+		if err != nil {
+			panic(err)
+		}
+		return &rpc.ReturnedObject{
+			FeatureID:     request.FeatureID,
+			ObjectContent: by,
+			Found:         true,
+		}, nil
+	}
+	panic("Unreachable")
+
+}
+
+func (r RouteService) ScanRegion(ctx context.Context, request *rpc.ScanRegionRequest) (*rpc.ObjectList, error) {
+	_, obj, _ := r.mapctx.ScanRegion(request.Lat, request.Lon, 4)
+	return &rpc.ObjectList{FeatureID: func() []string {
+		var ret []string
+		for _, v := range obj {
+			ret = append(ret, v.String())
+		}
+		return ret
+	}()}, nil
+}
+
+func (r RouteService) GetAssociatedObject(ctx context.Context, request *rpc.GetAssociatedObjectRequest) (*rpc.ObjectList, error) {
+	ids := r.mapctx.GetRelationByFeature(request.FeatureID)
+	return &rpc.ObjectList{FeatureID: func() []string {
+		var ret []string
+		for _, v := range ids {
+			ret = append(ret, v.String())
+		}
+		return ret
+	}()}, nil
+}
+
+func (r RouteService) SearchByNamePrefix(ctx context.Context, search *rpc.NameSearch) (*rpc.NameList, error) {
+	panic("implement me")
+}
+
+func (r RouteService) SearchByNameExact(ctx context.Context, search *rpc.NameSearch) (*rpc.ObjectList, error) {
+	panic("implement me")
+}
