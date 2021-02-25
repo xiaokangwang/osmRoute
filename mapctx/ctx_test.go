8bed462fd6f7070481ddc774b0ca5ccb6cc4ea48
Shelikhoo <xiaokangwang@outlook.com>
2021-02-25 17:46:28 +0000

    Test for bus routing

--- mapctx/ctx_test.go
+++ mapctx/ctx_test.go
@@ -3,6 +3,7 @@ package mapctx
 import (
 	"github.com/beefsack/go-astar"
 	"github.com/paulmach/osm"
+	"github.com/stretchr/testify/assert"
 	"github.com/xiaokangwang/osmRoute/adm"
 	"github.com/xiaokangwang/osmRoute/util"
 	"os"
@@ -87,6 +88,59 @@ func TestMapCtx_TryRoute(t *testing.T) {
 		println(v.(*NodeImpl).FeatureID().String())
 		last = v
 	}
+}
+
+func TestMapCtx_TryRouteWithBus(t *testing.T) {
+	mapinde := adm.GetMapFromDir(util.GetBaseDirFromEnvironment() + "/testdb")
+	mapfile, err := os.Open(util.GetBaseDirFromEnvironment() + "/ireland.osm.pbf")
+	if err != nil {
+		panic(err)
+	}
+	mapCtx := NewMapCtx(*mapinde, mapfile)
+
+	mapCtx.SetSpec(specDef{})
+
+	InitialNodes := mapCtx.GetNodeWithInterconnection(53.35214, -6.25866, specDef{})
+
+	for _, v := range InitialNodes {
+		node := v.(*NodeImpl)
+		println(node.FeatureID().String())
+	}
+
+	InitialNodesF := mapCtx.GetNodeWithInterconnection(53.36135, -6.23813, specDef{})
+
+	for _, v := range InitialNodesF {
+		node := v.(*NodeImpl)
+		println(node.FeatureID().String())
+	}
+	path, dist, found := astar.Path(InitialNodes[0], InitialNodesF[0])
+	println(found)
+	println(dist)
+	var last astar.Pather
+	reverseAny(path)
+	ok := false
+	for _, v := range path {
+		if last != nil {
+			fid := ""
+			ViaObject := last.(Node).PathNeighborVia(v)
+			viatype := ViaObject.ObjectID().Type()
+			switch viatype {
+			case osm.TypeWay:
+				infoway := (ViaObject).(*osm.Way)
+				fid = infoway.FeatureID().String()
+			case osm.TypeRelation:
+				inforela := (ViaObject).(*osm.Relation)
+				if inforela.Tags.Find("route") == "bus" {
+					ok = true
+				}
+				_ = inforela
+			}
+			println("via:", fid)
+		}
+		println(v.(*NodeImpl).FeatureID().String())
+		last = v
+	}
+	assert.True(t, ok, "Should have at least a bus route")
 }
 
 func reverseAny(s interface{}) {
